1c23efafde7620424b8f85607d4ac42cc58da0ee
spko <spko0321@gmail.com>
2025-06-04 19:56:04 +0900

    feat : 회원가입 postman test 완료

--- src/main/java/org/example/lifechart/domain/user/controller/UserController.java
+++ src/main/java/org/example/lifechart/domain/user/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.lifechart.domain.user.dto.SignupRequest;
 import org.example.lifechart.domain.user.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,7 +15,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody SignupRequest request) {
+    public ResponseEntity<?> signup(@RequestBody @Validated SignupRequest request) {
         userService.signup(request);
         return ResponseEntity.ok().build(); // 또는 id 반환 등
     }
