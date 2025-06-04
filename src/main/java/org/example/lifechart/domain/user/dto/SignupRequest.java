3ace8cd60c74665f53db83027ba1e4cac5288d22
spko <spko0321@gmail.com>
2025-06-04 15:22:04 +0900

    feat : 회원가입 요청 DTO 생성

---
+++ src/main/java/org/example/lifechart/domain/user/dto/SignupRequest.java
@@ -0,0 +1,21 @@
+package org.example.lifechart.domain.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class SignupRequest {
+
+    @Email(message = "이메일 형식이 아닙니다.")
+    @NotBlank(message = "이메일은 필수입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 4, message = "비밀번호는 최소 4자 이상이어야 합니다.")
+    private String password;
+
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickname;
+}
