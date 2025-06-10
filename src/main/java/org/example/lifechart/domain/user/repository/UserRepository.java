d75724bfd78b71d713408c5a068f9e1f4d371734
spko <spko0321@gmail.com>
2025-06-11 07:01:02 +0900

    feat : 회원탈퇴(softdelete)후 30일 지나면 완전 삭제 (스케쥴링) 기능 추가 + 유저 정보조회 관련 SuccessCode 추가

--- src/main/java/org/example/lifechart/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/lifechart/domain/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package org.example.lifechart.domain.user.repository;
 import org.example.lifechart.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,6 +12,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
     boolean existsByEmailAndIsDeletedTrue(String email);
+    List<User> findAllByIsDeletedTrueAndDeletedAtBefore(LocalDateTime deadline);// 30일 넘은 soft deleted 유저 찾기
+
 }
 
 
