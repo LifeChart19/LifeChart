2a9b71d867606a7e19bd990e4957a7e4e74a05b8
le951 <ihyeongjin043@gmail.com>
2025-06-11 11:09:02 +0900

    Feat : add Aws Config (Beans)

---
+++ src/main/java/org/example/lifechart/common/config/AwsConfig.java
@@ -0,0 +1,57 @@
+package org.example.lifechart.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
+
+
+@Configuration
+public class AwsConfig {
+
+    @Value("${aws.arn.role.sqssns}")
+    private String ARN_ROLE_SQSSNS;
+
+
+    @Bean
+    public StsClient stsClient() {
+        return StsClient.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public SqsClient sqsClient(StsClient stsClient){
+        return SqsClient.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(StsAssumeRoleCredentialsProvider.builder()
+                        .stsClient(stsClient)
+                        .refreshRequest(r -> r
+                                .roleArn(ARN_ROLE_SQSSNS)
+                                .roleSessionName("sqs"))
+                        .build()
+                )
+                .build();
+    }
+
+    @Bean
+    public SnsClient snsClient(StsClient stsClient){
+        return SnsClient.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(StsAssumeRoleCredentialsProvider.builder()
+                        .stsClient(stsClient)
+                        .refreshRequest(r -> r
+                                .roleArn(ARN_ROLE_SQSSNS)
+                                .roleSessionName("sns-pub"))
+                        .build()
+                )
+                .build();
+    }
+
+}
