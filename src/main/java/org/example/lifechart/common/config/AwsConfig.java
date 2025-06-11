07e0278c32eec605177e562b01131fb48945179c
le951 <ihyeongjin043@gmail.com>
2025-06-12 07:27:13 +0900

    fix : AwsConfig / secret.example.properties

--- src/main/java/org/example/lifechart/common/config/AwsConfig.java
+++ src/main/java/org/example/lifechart/common/config/AwsConfig.java
@@ -1,10 +1,16 @@
 package org.example.lifechart.common.config;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
@@ -14,14 +20,42 @@ import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider
 @Configuration
 public class AwsConfig {
 
-	@Value("${aws.arn.role.sqssns}")
-	private String ARN_ROLE_SQSSNS;
+	@Value("${aws.arn.role.message}")
+	private String arnRoleMessage;
+
+	@Value("${aws.key.access.id}")
+	private String accessId;
+
+	@Value("${aws.key.access.secret}")
+	private String secretAccessKey;
+
+	private boolean isRunningOnEc2() {
+		try {
+			HttpURLConnection connection = (HttpURLConnection) new URL(
+				"http://169.254.169.254/latest/meta-data/").openConnection();
+			connection.setConnectTimeout(100); // 매우 짧게 설정
+			connection.connect();
+			return connection.getResponseCode() == 200;
+		} catch (IOException e) {
+			return false;
+		}
+	}
 
 	@Bean
 	public StsClient stsClient() {
+
+		if(isRunningOnEc2()){
+			return StsClient.builder()
+				.region(Region.AP_NORTHEAST_2)
+				.credentialsProvider(DefaultCredentialsProvider.create())
+				.build();
+		}
+
+		AwsBasicCredentials credentials = AwsBasicCredentials.create(accessId, secretAccessKey);
+
 		return StsClient.builder()
 			.region(Region.AP_NORTHEAST_2)
-			.credentialsProvider(DefaultCredentialsProvider.create())
+			.credentialsProvider(StaticCredentialsProvider.create(credentials))
 			.build();
 	}
 
@@ -32,7 +66,7 @@ public class AwsConfig {
 			.credentialsProvider(StsAssumeRoleCredentialsProvider.builder()
 				.stsClient(stsClient)
 				.refreshRequest(r -> r
-					.roleArn(ARN_ROLE_SQSSNS)
+					.roleArn(arnRoleMessage)
 					.roleSessionName("sqs"))
 				.build()
 			)
@@ -46,7 +80,7 @@ public class AwsConfig {
 			.credentialsProvider(StsAssumeRoleCredentialsProvider.builder()
 				.stsClient(stsClient)
 				.refreshRequest(r -> r
-					.roleArn(ARN_ROLE_SQSSNS)
+					.roleArn(arnRoleMessage)
 					.roleSessionName("sns-pub"))
 				.build()
 			)
