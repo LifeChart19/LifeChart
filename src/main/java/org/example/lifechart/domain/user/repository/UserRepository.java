99b21b6bb5d10cfd5924a02fd261b86b2a2a7436
EZRANDO <gpdnjs2578@naver.com>
2025-06-16 18:47:10 +0900

    [Feat] : simulation수정사항 추가

--- src/main/java/org/example/lifechart/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/lifechart/domain/user/repository/UserRepository.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 
 import org.example.lifechart.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
