1dd69c6c87da87aea54c2647fa11cc149b7d86d6
EZRANDO <gpdnjs2578@naver.com>
2025-06-24 18:42:23 +0900

    [feat] simulation피드백 반영

--- src/main/java/org/example/lifechart/domain/simulation/service/simulation/SimulationServiceImpl.java
+++ src/main/java/org/example/lifechart/domain/simulation/service/simulation/SimulationServiceImpl.java
@@ -163,13 +163,12 @@ public class SimulationServiceImpl implements SimulationService {
     @Transactional
     public void updateSimulationsByGoalChange(Long userId, Long goalId) {
 
-        userRepository.findByIdAndDeletedAtIsNull(userId)
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-
+        User user = validUser(userId);
+        Goal goal = validGoal(goalId, user.getId());
         // 시뮬레이션 골에서 ACTIVE인 것만
         //goalid에 연결된 시뮬레이션id를 갖고와야함.
         List<SimulationGoal> simulationGoals =
-                simulationGoalRepository.findAllByGoalIdAndUserIdAndActiveTrue(goalId, userId);
+                simulationGoalRepository.findAllByGoalIdAndUserIdAndActiveTrue(user.getId(), goal.getId());
 
         if (simulationGoals.isEmpty()) {
             throw new CustomException(ErrorCode.SIMULATION_NOT_FOUND_BY_GOAL);
@@ -304,5 +303,16 @@ public class SimulationServiceImpl implements SimulationService {
         }
 
         simulationRepository.delete(simulation);
+    }
+
+    private User validUser(Long userId) {
+        User user = userRepository.findByIdAndDeletedAtIsNull(userId)
+                .orElseThrow(()-> new CustomException(ErrorCode.USER_NOT_FOUND));
+        return user;
+    }
+    private Goal validGoal(Long goalId, Long userId) {
+        Goal goal = goalRepository.findByIdAndUserId(goalId, userId).
+                orElseThrow(()-> new CustomException(ErrorCode.GOAL_NOT_FOUND));
+        return goal;
     }
 }
