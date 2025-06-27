cc69dd9b273928425a9058145cf1e84859033055
spko <spko0321@gmail.com>
2025-06-27 18:50:34 +0900

    refactor: Account 엔티티 주요 필드 @Column(nullable = false) 추가

--- src/main/java/org/example/lifechart/domain/account/entity/Account.java
+++ src/main/java/org/example/lifechart/domain/account/entity/Account.java
@@ -12,11 +12,21 @@ import java.time.LocalDateTime;
 @Builder
 public class Account {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
 
+    @Column(nullable = false)
     private Long userId; // 소유자 userId (연관관계는 단방향)
+
+    @Column(nullable = false)
     private String accountNumber;
+
+    @Column(nullable = false)
     private String accountName;
+
+    @Column(nullable = false)
     private BigDecimal balance;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 }
