882feef7f7497a045cf8af03305bce5ef1d56c55
spko <spko0321@gmail.com>
2025-06-29 15:17:18 +0900

    refactor : deposit, withdrawl enum 타입으로 변경

--- src/main/java/org/example/lifechart/domain/account/dto/TransactionResponse.java
+++ src/main/java/org/example/lifechart/domain/account/dto/TransactionResponse.java
@@ -1,6 +1,7 @@
 package org.example.lifechart.domain.account.dto;
 
 import lombok.Getter;
+import org.example.lifechart.domain.account.enums.TransactionType;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -9,7 +10,7 @@ import java.time.LocalDateTime;
 public class TransactionResponse {
     private Long id;
     private BigDecimal amount;
-    private String type; // DEPOSIT, WITHDRAWAL
+    private TransactionType type; // DEPOSIT, WITHDRAWAL
     private LocalDateTime createdAt;
     private String description;
     private String memo;
