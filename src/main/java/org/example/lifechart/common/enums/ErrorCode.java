b6093bc8a1f09791706e5289d5f206a87b946786
obt <byeongtaek12@naver.com>
2025-06-30 18:00:09 +0900

    [feat] : 자동완성 기능 구현 (#85) [refactor] 태그 리팩토링, [test] : 테스트 코드 추가 1. ErrorCode 에러코드 추가 2. ShareGoalController 클래스 기능 추가 3. ShareGoalSearchRequestDto 클래스 필드 추가 4. ShareGoalService 인터페이스 메서드 추가 5. ShareGoalServiceImpl 메서드 추가 및 태그 리팩토링 6. SuccessCode 성공코드 추가 7. TagValidator 클래스 로직 추가 8. ValidTags 사용자 정의 어노테이션 message 수정 9. ShareGoalServiceImplTest 테스트 코드 리팩토링 및 테스트코드 추가 기능: 자동완성API 추가

--- src/main/java/org/example/lifechart/common/enums/ErrorCode.java
+++ src/main/java/org/example/lifechart/common/enums/ErrorCode.java
@@ -156,6 +156,7 @@ public enum ErrorCode implements BaseCode {
 
 	// shareGoal (Line#: 157~169)
 	SHARE_GOAL_KEYWORD_BAD_REQUEST(HttpStatus.BAD_REQUEST, "키워드는 공백이면 안 되고 길이가 2이상이여야 합니다"),
+	SHARE_GOAL_PREFIX_BAD_REQUEST(HttpStatus.BAD_REQUEST, "prefix는 null이거나 공백이면 안 됩니다."),
 
 
 
--- src/main/java/org/example/lifechart/validation/annotation/ValidTags.java
+++ src/main/java/org/example/lifechart/validation/annotation/ValidTags.java
@@ -16,7 +16,7 @@ import jakarta.validation.Payload;
 @Constraint(validatedBy = TagsValidator.class)
 @Documented
 public @interface ValidTags {
-	String message() default "태그는 명사로만 작성해야 하고, 제목의 주요 키워드들을 모두 포함해야 하며, "
+	String message() default "태그는 명사로만 작성해야 하고, 제목의 주요 키워드를 반드시 하나 포함해야 하며, "
 		+ "(한글, 영문, 숫자)로만 구성되어야 합니다";
 	Class<?>[] groups() default {};
 	Class<? extends Payload>[] payload() default {};
